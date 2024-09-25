f606daa21ef8358c7a6904f9bb22cf92b86502fa
Barry Nie <23257024@life.hkbu.edu.hk>
2024-09-25 17:42:35 +0800

    Add files via upload

--- lab03_partB/src/charFinder.java
+++ lab03_partB/src/charFinder.java
@@ -6,16 +6,16 @@ public class charFinder {
         System.out.print("Enter a line:");
         String line = in.nextLine();
         System.out.print("Enter a character:");
-        String character = in.nextLine();
+        char character = in.nextLine().charAt(0);
 
         int count = 0;
         for(int i = 0;i<line.length();i++){
-            if(character.charAt(0) == line.charAt(i)){
+            if(character == line.charAt(i) || character == (line.charAt(i)-'A'+'a')){
                 count++;
                 System.out.print("_");
             }
             else System.out.print(line.charAt(i));
         }
-        System.out.printf("\nNo. of occurrences of %s:%d",character,count);
+        System.out.printf("\nNo. of occurrences of %c:%d",character,count);
     }
 }
